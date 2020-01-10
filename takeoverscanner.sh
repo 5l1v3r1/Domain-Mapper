e0c7defcbe8b5d85c2c8b07ecfecd30ee3bcdbfe
Haroon Awan <mrharoonawan@Gmail.com>
2020-01-10 11:58:27 -0500

    Update takeoverscanner.sh

--- takeoverscanner.sh
+++ takeoverscanner.sh
@@ -5,6 +5,7 @@ echo " [+] New file will be created as complete subdomains list"
 echo " [+] File will provide fullest possible information of takeovers"
 echo " [+] Output file will show various services famous to unknown" 
 echo " [+] Services that are vulernable to takeovers"
-echo " [+] Enter file path: "
-read $Target
-dig -f $Target 2> /dev/null | awk '/CNAME/ {print}' > takeoverlist_complete_subdomains.txt
+echo -ne " [+] Enter file path: "
+read Target
+dig -f $Target 2> /dev/null | awk "/CNAME/ {print}" > takeoverlist_complete_subdomains.txt
+
